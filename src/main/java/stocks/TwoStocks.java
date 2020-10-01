7b58845b2669e4c20aaf2ee38f3123c5222d8c9e
Bence Gaizer <bencegaizer@gmail.com>
2020-10-01 10:49:32 +0000

    Updated at 2020-10-01 10:49:33 UTC

--- src/main/java/stocks/TwoStocks.java
+++ src/main/java/stocks/TwoStocks.java
@@ -2,10 +2,28 @@ package stocks;
 
 import java.util.List;
 
+public class opportunity{
+	public int buyTime;
+    public int sellTime;
+    public int profit;
+    public Boolean firstStock
+    
+    public opportunity(int buyTime, int sellTime, Boolean firstStock,
+                       List<Integer> firstStockPrices, List<Integer> secondStockPrices){
+    	this.buyTime = buyTime;
+        this.sellTime = sellTime;
+        this.firstStock = firstStock;
+        
+        if (firstStock)
+            this.profit = firstStockPrices[sellTime] - firstStockPrices[buyTime];
+		else
+            this.profit = secondStockPrices[sellTime] - secondStockPrices[buyTime];
+    }
+}
+
 public class TwoStocks {
     public int calculateProfit(List<Integer> firstStockPrices, List<Integer> secondStockPrices) {
-    	throw new UnsupportedOperationException("Function not yet implemented.");
-
+    	List<opportunity> Opportunities = new ArrayList<opportunity>();
     }
 }
 
