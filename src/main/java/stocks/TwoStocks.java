0381dede09ce80d713834733428695eadf453a0f
Bence Gaizer <bencegaizer@gmail.com>
2020-10-01 10:49:55 +0000

    Updated at 2020-10-01 10:49:56 UTC

--- src/main/java/stocks/TwoStocks.java
+++ src/main/java/stocks/TwoStocks.java
@@ -6,7 +6,7 @@ public class opportunity{
 	public int buyTime;
     public int sellTime;
     public int profit;
-    public Boolean firstStock
+    public Boolean firstStock;
     
     public opportunity(int buyTime, int sellTime, Boolean firstStock,
                        List<Integer> firstStockPrices, List<Integer> secondStockPrices){
