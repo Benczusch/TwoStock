67482c70a51c13d168f9d46fe4ba6b851638cfe3
Cambridge Spark <tech@cambridgespark.com>
2020-10-01 10:30:08 +0000

    Initialise repo [skip-ci]

---
+++ src/main/java/stocks/TwoStocks.java
@@ -0,0 +1,11 @@
+package stocks;
+
+import java.util.List;
+
+public class TwoStocks {
+    public int calculateProfit(List<Integer> firstStockPrices, List<Integer> secondStockPrices) {
+    	throw new UnsupportedOperationException("Function not yet implemented.");
+
+    }
+}
+
